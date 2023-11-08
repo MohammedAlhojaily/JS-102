dbd1c4703ee4d6ae2d2453e83a2a3913cb56c814
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-08 21:34:26 +0300

    Arrow Functions with CallBack

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-//function sum(firstNumber, secondNumber){
-  //  return firstNumber + secondNumber
-//}
-function sub(firstNumber, secondNumber){
-    return firstNumber - secondNumber
-}
-
-function mul(firstNumber, secondNumber){
-    return firstNumber * secondNumber
-}
 
 function operation(firstNumber, secondNumber, opCallback){
     return `[${opCallback(firstNumber, secondNumber)}]`
 }
 
-console.log(operation(5,2, function(firstNumber, secondNumber){
-    return firstNumber + secondNumber
-}))
+console.log(operation(5,2,(firstNumber, secondNumber) => firstNumber + secondNumber))
