b0b849a5d897f14e7a909d8eeaa420f28ba614b5
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-08 22:33:07 +0300

    reduce

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const strColors = 'red ; green ; blue ; black'
+const numbers = [2, 4, 1, 3, 5]
 
-const arrColors = strColors.split(' ; ', 3)
-console.log(arrColors);
+const total = numbers.reduce(function(accumulator, currentValue, index, array){
+    return accumulator + currentValue
+}, 100)
+console.log(total)
