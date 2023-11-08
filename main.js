4f2059f4a5ea212c5fbc3a049f58acdeda72e1e9
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-08 21:56:07 +0300

    findIndex

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const colors = ['red', 'green', 'blue', 'green']
+const ages = [12, 14, 19, 21]
 
-const index = colors.lastIndexOf('green', -2)
+const index = ages.findIndex(function(element){
+    return element >=25
+})
 console.log(index)
