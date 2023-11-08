3347567598cf9da854d4a3f51eb45f91d29ac02f
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-08 21:48:19 +0300

    some

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const colors = ['red', 'green', 'blue']
+const ages = [11, 13, 15, 12]
 
-colors.forEach(function(currentValue, index, array){
-    console.log(index, currentValue);
+const hasAdult = ages.some(function(element){
+    return element >= 18
 })
+
+console.log(hasAdult)
