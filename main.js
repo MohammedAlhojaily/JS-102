26996ba24342ce56e835eb9cb16121a2c26be322
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-09 21:28:33 +0300

    Wrtie functions in objects

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-(function(n){
-    console.log(n);
-})('Reem');
+let obj = {
+    title: "How to write a function in an object",
 
+    function1 : function(){
+        return console.log("first function");
+    },
+    function2: () => {
+        return console.log("second function");
+    },
+    function3(){
+        return console.log("third function");
+    }
+}
+
+obj.function1();
+obj.function2();
+obj.function3();
