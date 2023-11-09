83626fb8e506143d07cfd8b70af7018a9a3682f7
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-09 17:59:06 +0300

    methods in class

--- main.js
+++ main.js
@@ -4,9 +4,17 @@ class Car {
         this.color = color
         this.currentSpeed = currentSpeed
     }
+    move(speed){
+        this.currentSpeed = speed
+        return `The ${this.name} is moving at speed of ${this.currentSpeed}km per hour`
+    }
+    stop(){
+        this.currentSpeed = 0
+        return `The ${this.name} stopped`
+    }
 }
 
 const car1 = new Car('camry', 'white', 0)
 const car2 = new Car('corella', 'black',  0) 
-console.log(car1);
-console.log(car2);
+console.log(car1.move(50));
+console.log(car2.move(70));
