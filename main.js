41d7006840871b44b28a251bc7632aa082eaedfe
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-09 17:53:23 +0300

    this keyword

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const car = {
     name: 'camry',
     color: 'white',
     currentSpeed: 0,
-    move: function(speed){},
+    move: function(speed){
+       this.currentSpeed = speed
+       return `The ${this.name} is moving at speed of ${this.currentSpeed}km per hour`
+    },
     stop: function(){
-        console.log('stop method');
+        this.currentSpeed = 0
+        return `The ${this.name} stopped`
     }
 }
 
-console.log(car.name)
-car.stop()
+console.log(car.move(100));
