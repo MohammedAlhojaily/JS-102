0221f4993edcb155a0846cb39f499032488b8363
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-09 21:31:06 +0300

    Passing More/less Arguments

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
-let obj = {
-    title: "How to write a function in an object",
-
-    function1 : function(){
-        return console.log("first function");
-    },
-    function2: () => {
-        return console.log("second function");
-    },
-    function3(){
-        return console.log("third function");
-    }
+function rectangleArea(w, l){
+    console.log(w, l);
+    let a = w * l;
+    return a;
 }
 
-obj.function1();
-obj.function2();
-obj.function3();
+let area = rectangleArea(4);
+console.log(area);
